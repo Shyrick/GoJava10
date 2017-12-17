8f45fa1c4383fefe12407601c28d65b4bf8e7432
Shyrick <lykun@mail.ru>
2017-12-17 21:17:32 +0300

    1

---
+++ src/First.java
@@ -0,0 +1,10 @@
+public class First {
+
+    public static void main(String[] args) {
+
+        System.out.println("Строка 1");
+        System.out.println("Строка 2\nСтрока 3\tСтрока 4");
+    }
+
+
+}
