cd2fab2cd1dcda2f671eddcf3cb628c120d9a8f5
Shyrick <lykun@mail.ru>
2018-01-05 04:00:26 +0300

    Елочка

---
+++ src/HW1/Task1_2.java
@@ -0,0 +1,16 @@
+package HW1;
+
+public class Task1_2 {
+
+    public static void main(String[] args) {
+        System.out.println("      /\\  ");
+        System.out.println("     /  \\ ");
+        System.out.println("    / || \\");
+        System.out.println("   /  ||  \\");
+        System.out.println("  /   ||   \\  ");
+        System.out.println(" /    ||    \\  ");
+        System.out.println("/     ||     \\  ");
+        System.out.println("      ||        ");
+        System.out.println("----------------");
+    }
+}
